9a45b1a1096d14a50102d7282483a082b6afd6aa
Davin K. Tanabe <davin.tanabe@digitalasset.com>
2026-03-04 08:10:27 -0500

    Make Osano only active on the production site, and not pull-request builds or when running locally.

--- osano.js
+++ osano.js
@@ -1,3 +1,5 @@
-var script = document.createElement("script");
-script.src = "https://cmp.osano.com/16A4BWTQ2aBjp3tNZ/54d97448-ff70-4b63-aaff-7dcf04da4e6c/osano.js";
-document.head.appendChild(script);
+if (window.location.hostname === "docs.canton.network") {
+  var script = document.createElement("script");
+  script.src = "https://cmp.osano.com/16A4BWTQ2aBjp3tNZ/54d97448-ff70-4b63-aaff-7dcf04da4e6c/osano.js";
+  document.head.appendChild(script);
+}
