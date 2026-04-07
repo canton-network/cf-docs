318479adca9971807c3db0879b6058432bc9e9fb
danielporterda <daniel.porter@digitalasset.com>
2026-04-07 13:35:59 -0400

    Update JSON API TOC layout

--- shell.nix
+++ shell.nix
@@ -4,19 +4,19 @@ let
   python = pkgs.python311;
   x2mdx = python.pkgs.buildPythonApplication rec {
     pname = "x2mdx";
-    version = "0.1.0+git-1a8a0c4";
+    version = "0.1.0+git-6db616f";
     pyproject = true;
-    src = pkgs.fetchFromGitHub {
-      owner = "danielporterda";
-      repo = "x2mdx";
-      rev = "1a8a0c44790f7cfaf26407b41c371cc2cbb02a64";
-      sha256 = "sha256-GUOx27QBFgZKDIhaPrUfrsNuclDKTSyEy0EQvUM0CDw=";
+    src = builtins.fetchGit {
+      url = "https://github.com/danielporterda/x2mdx.git";
+      rev = "6db616fcc48fdf1e2a15ad0972a07553a1dea7c4";
     };
     nativeBuildInputs = with python.pkgs; [
       setuptools
       wheel
     ];
     propagatedBuildInputs = with python.pkgs; [
+      jinja2
+      protobuf
       pyyaml
     ];
     doCheck = false;
