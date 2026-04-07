10cb3ef0a06d75452fb85601d98be2cc9104db77
Daniel Porter <daniel.porter@digitalasset.com>
2026-04-07 14:44:24 -0400

    Add JSON API AsyncAPI reference generator (#93)

--- shell.nix
+++ shell.nix
@@ -4,17 +4,18 @@ let
   python = pkgs.python311;
   x2mdx = python.pkgs.buildPythonApplication rec {
     pname = "x2mdx";
-    version = "0.1.0+git-df446df";
+    version = "0.1.0+git-6db616f";
     pyproject = true;
     src = builtins.fetchGit {
       url = "https://github.com/danielporterda/x2mdx.git";
-      rev = "df446df4e3ca89af5c557787d34628b13ffc7e12";
+      rev = "6db616fcc48fdf1e2a15ad0972a07553a1dea7c4";
     };
     nativeBuildInputs = with python.pkgs; [
       setuptools
       wheel
     ];
     propagatedBuildInputs = with python.pkgs; [
+      jinja2
       protobuf
       pyyaml
     ];
