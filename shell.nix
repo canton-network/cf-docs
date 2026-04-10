22bf7d1348be83400ae4e9fadd2d3c9ecd4c2bbe
danielporterda <daniel.porter@digitalasset.com>
2026-04-10 00:12:16 -0400

    Apply reviewed reference docs cleanup pass

--- shell.nix
+++ shell.nix
@@ -4,11 +4,11 @@ let
   python = pkgs.python311;
   x2mdx = python.pkgs.buildPythonApplication rec {
     pname = "x2mdx";
-    version = "0.1.0+git-4113a05";
+    version = "0.1.0+git-5ef8642";
     pyproject = true;
     src = builtins.fetchGit {
       url = "https://github.com/danielporterda/x2mdx.git";
-      rev = "4113a05ff41f4bde1f2204fd3cc1e6f82e65ca0e";
+      rev = "5ef86425194a99c64e1f86c9f07d71e6e8825b91";
     };
     nativeBuildInputs = with python.pkgs; [
       setuptools
