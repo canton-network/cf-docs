66f29f0cff9159952ab8ef62eb0188b2a04e2365
Ibo Sy <ibo@b9lab.com>
2026-05-18 12:39:40 +0200

    update osano script, add google analytics loader
    Signed-off-by: Ibo Sy <ibo@b9lab.com>

--- docs-main/osano.js
+++ docs-main/osano.js
@@ -1,3 +1,41 @@
-var script = document.createElement("script");
-script.src = "https://cmp.osano.com/16A4BWTQ2aBjp3tNZ/54d97448-ff70-4b63-aaff-7dcf04da4e6c/osano.js";
-document.head.appendChild(script);
+(function (w, o, d) {
+    w[o] =
+        w[o] ||
+        function () {
+            w[o][d].push(arguments);
+        };
+    w[o][d] = w[o][d] || [];
+})(window, 'Osano', 'data');
+
+window.Osano('onAnalytics', consent => {
+    loadGoogleTag();
+});
+
+(function loadOsano() {
+    const osanoScript = document.createElement('script');
+    osanoScript.src = 'https://cmp.osano.com/16A4BWTQ2aBjp3tNZ/54d97448-ff70-4b63-aaff-7dcf04da4e6c/osano.js';
+    osanoScript.async = true;
+    document.head.appendChild(osanoScript);
+
+  })();
+
+function loadGoogleTag() {
+    const gtagScript = document.createElement('script');
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-F2G1L137JX';
+    gtagScript.async = true;
+  
+    gtagScript.onload = function () {
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+        dataLayer.push(arguments);
+        }
+
+        window.gtag = gtag;
+
+        gtag('js', new Date());
+        gtag('config', 'G-F2G1L137JX');
+    };
+  
+    document.head.appendChild(gtagScript);
+}
