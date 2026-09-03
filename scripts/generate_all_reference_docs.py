a2d81d5d214d95998daa5c2eeadc88fb61ff68ea
danielporterda <daniel.porter@digitalasset.com>
2026-09-03 11:12:49 -0400

    Add Canton topology version generator
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>

--- scripts/generate_all_reference_docs.py
+++ scripts/generate_all_reference_docs.py
@@ -116,6 +116,11 @@ SCRIPT_JOBS = [
         nav_slices=(),
         target_ids=(),
     ),
+    ScriptJob(
+        script_path=REPO_ROOT / "scripts" / "generate_canton_topology_transaction_versions.py",
+        nav_slices=(),
+        target_ids=(),
+    ),
 ]
 
 
