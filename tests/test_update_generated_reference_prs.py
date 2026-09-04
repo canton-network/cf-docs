4c9272142ad62bb68dd05e1a21ce062a327b50c4
Daniel Porter <daniel.porter@digitalasset.com>
2026-09-04 11:23:18 -0400

    Publish Canton release and protocol compatibility (#1548)
    * Add Canton release protocol generator
    
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>
    
    * Publish Canton protocol compatibility table
    
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>
    
    * Add protocol compatibility preview screenshots
    
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>
    
    * Share the Canton release artifact cache
    
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>
    
    * Rename protocol compatibility navigation entry
    
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>
    
    * Refresh protocol compatibility preview
    
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>
    
    * Refresh Canton protocol table to 3.5.16
    
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>
    
    ---------
    
    Signed-off-by: danielporterda <daniel.porter@digitalasset.com>

--- tests/test_update_generated_reference_prs.py
+++ tests/test_update_generated_reference_prs.py
@@ -60,6 +60,7 @@ def test_update_targets_cover_all_generated_doc_surfaces() -> None:
         "typescript-bindings",
         "canton-console-reference",
         "canton-error-codes-reference",
+        "canton-release-protocol-versions",
         "canton-metrics-reference",
         "canton-topology-proto-link",
         "canton-release-notes",
@@ -373,6 +374,7 @@ def test_generated_clean_paths_include_target_paths_and_internal_output() -> Non
     assert "docs-main/global-synchronizer/deployment/validator-kubernetes.mdx" in clean_paths
     assert "docs-main/global-synchronizer/reference/canton-console-commands.mdx" in clean_paths
     assert "docs-main/global-synchronizer/reference/error-codes.mdx" in clean_paths
+    assert "docs-main/release-notes/releases-and-versioning.mdx" in clean_paths
     assert "docs-main/global-synchronizer/reference/canton-metrics.mdx" in clean_paths
     assert "docs-main/global-synchronizer/release-notes" in clean_paths
     assert "docs-main/integrations/release-notes/wallet-gateway.mdx" in clean_paths
